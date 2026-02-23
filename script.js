86f1117f7a30b6499e0773c234f2f84496a63a2a
Md Mahedi Hasan <mdmahedi6427@gmail.com>
2026-02-23 13:13:49 +0600

    Feat: implement toggleStyle function for active filter button styling

--- script.js
+++ script.js
@@ -18,5 +18,28 @@ const allCardSection = document.getElementById('all-cards');
 // const totalJobs = allCardSection.children.length;
 //  console.log(allCardSection.children.length)
 
+const mainContainer = document.querySelector('main');
 
-f
+function calculateCount() {
+    const currentTotal = allCardSection.children.length;
+    // let total = allCardSection.children.length;
+    total.innerText = currentTotal;
+    // countJobs.innerText = allCardSection.children.length;
+
+
+    if (currentStatus === 'interview-filter-btn') {
+        countJobs.innerText = `${interviewList.length} of ${currentTotal}`;
+    }
+    else if (currentStatus === 'rejected-filter-btn') {
+        countJobs.innerText = `${rejectedList.length} of ${currentTotal}`;
+    }
+    else {
+        countJobs.innerText = currentTotal;
+    }
+
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
+}
+
+
+calculateCount();
