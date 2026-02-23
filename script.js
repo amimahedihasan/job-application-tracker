5c704e175fd339cea15f78934dbf7bc0a47071be
Md Mahedi Hasan <mdmahedi6427@gmail.com>
2026-02-23 13:13:24 +0600

    Feat: add calculateCount function to update real-time statistics

---
+++ script.js
@@ -0,0 +1,22 @@
+let rejectedList = [];
+let interviewList = [];
+let currentStatus = 'all'
+
+
+
+let total = document.getElementById('total-count');
+let interviewCount = document.getElementById('interview-count');
+let rejectedCount = document.getElementById('rejected-count');
+let countJobs = document.getElementById('count-jobs');
+
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
+const filteredSection = document.getElementById('filtered-section');
+
+const allCardSection = document.getElementById('all-cards');
+// const totalJobs = allCardSection.children.length;
+//  console.log(allCardSection.children.length)
+
+
+f
